2e4757be7108f2097cd0aef65b5f48758a3ae75d
NeutralMilk <c14509343@dit.ie>
2017-03-22 12:17:56 +0000

    Fixed code

--- app/src/main/java/colin/personalplan/Categories.java
+++ app/src/main/java/colin/personalplan/Categories.java
@@ -1,16 +1,11 @@
 package colin.personalplan;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -34,9 +29,6 @@ public class Categories extends ListActivity implements TextToSpeech.OnInitListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories);
-
-        //Resources res = getResources();
-
 
         Integer[] imgid =
         {
